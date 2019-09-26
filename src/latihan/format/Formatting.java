bf20924d5d33621da2aa704c28c4febf442afc4a
Wildhevire <wildhevire404@gmail.com>
2019-09-26 17:55:17 +0000

    finished

---
+++ src/latihan/format/Formatting.java
@@ -0,0 +1,22 @@
+package latihan.format;
+
+public class Formatting {
+	public static void main(String[] args) {
+        int i = 3456764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d&n", i);
+        System.out.printf("%%+10d : %+10d&n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+
+}
